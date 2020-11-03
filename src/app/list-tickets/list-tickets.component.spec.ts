ca2ae986f2c925d377d57a69aaf7ac6618c8756e
Remy <r.fevre184@gmail.com>
2020-11-03 10:31:27 +0100

    fix(): quelques fixs

--- src/app/list-tickets/list-tickets.component.spec.ts
+++ src/app/list-tickets/list-tickets.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { Ticket } from 'src/interfaces/ticket.interface';
 import { BackendService } from '../backend.service';
 import { FilterTycketByIdPipe } from '../pipe/filter-ticket-by-id.pipe';
 import { ListTicketsComponent } from './list-tickets.component';
@@ -9,10 +10,23 @@ describe('ListTicketsComponent', () => {
   let fixture: ComponentFixture<ListTicketsComponent>;
   let mockBackendService;
 
+  let user = {
+    id: 0,
+    name: 'testName'
+  };
+
+  
+  let ticket: Ticket = {
+    assigneeId: 0,
+    completed: false,
+    description: 'test',
+    id: 0
+  }
+
   beforeEach(async () => {
     mockBackendService = jasmine.createSpyObj(['tickets', 'users']);
-    mockBackendService.tickets.and.returnValue(of([]));
-    mockBackendService.users.and.returnValue(of([]));
+    mockBackendService.tickets.and.returnValue(of([ticket]));
+    mockBackendService.users.and.returnValue(of([user]));
 
     await TestBed.configureTestingModule({
       declarations: [ListTicketsComponent, FilterTycketByIdPipe],
@@ -31,5 +45,9 @@ describe('ListTicketsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load all tickets', () => {
+    expect(mockBackendService.tickets).toHaveBeenCalled();
   });
 });
